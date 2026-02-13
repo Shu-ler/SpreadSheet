181c93bc9c6813190da4947db8cf602c765558c7
Shu-ler <shuler1970@gmail.com>
2026-02-13 16:53:58 +0700

    Рефакторинг cell.*

--- cell.cpp
+++ cell.cpp
@@ -6,102 +6,115 @@
 #include <optional>
 #include <memory>
 
-class Cell::Impl {
-public:
-    virtual ~Impl() = default;
-    virtual Value GetValue() const = 0;
-    virtual std::string GetText() const = 0;
-};
+/*
+ * Cell
+ */
 
-class Cell::EmptyImpl : public Cell::Impl {
-public:
-    Value GetValue() const override {
-        return "";
-    }
-    
-    std::string GetText() const override {
-        return "";
-    }
-};
-
-class Cell::TextImpl : public Cell::Impl {
-public:
-    TextImpl(std::string text) : text_(std::move(text)) {}
-
-    Value GetValue() const override {
-        if (text_.empty()) {
-            return "";
-        }
-        if (text_[0] == ESCAPE_SIGN) {
-            return text_.substr(1);
-        }
-        return text_;
-    }
-    
-    std::string GetText() const override {
-        return text_;
-    }
-    
-private:
-    std::string text_;
-};
-
-class Cell::FormulaImpl : public Cell::Impl {
-public:
-    FormulaImpl(std::string expression) : formula_(ParseFormula(std::move(expression))) {}
-
-    Value GetValue() const override {
-        auto value = formula_->Evaluate();
-        if (std::holds_alternative<double>(value)) {
-            return std::get<double>(value);
-        } else {
-            return std::get<FormulaError>(value);
-        }
-    }
-    
-    std::string GetText() const override {
-        return FORMULA_SIGN + formula_->GetExpression();
-    }
-    
-private:
-    std::unique_ptr<FormulaInterface> formula_;
-};
-
-Cell::Cell() : impl_(std::make_unique<EmptyImpl>()) {}
-
-Cell::~Cell() = default;
+Cell::Cell(Sheet& sheet)
+	: impl_(std::make_unique<EmptyImpl>()) {
+}
 
 void Cell::Set(std::string text) {
-    if (text.empty()) {
-        impl_ = std::make_unique<EmptyImpl>();
-        return;
-    }
-    
-    if (text[0] == FORMULA_SIGN) {
-        if (text.size() == 1) {
-            impl_ = std::make_unique<TextImpl>(std::move(text));
-            return;
-        }
-        try {
-            auto formula = ParseFormula(text.substr(1));
-            impl_ = std::make_unique<FormulaImpl>(text.substr(1));
-        } catch (const FormulaException&) {
-            throw;
-        }
-        return;
-    }
-    
-    impl_ = std::make_unique<TextImpl>(std::move(text));
+	if (text.empty()) {
+		impl_ = std::make_unique<EmptyImpl>();
+		return;
+	}
+
+	if (text[0] == FORMULA_SIGN) {
+		if (text.size() == 1) {
+			impl_ = std::make_unique<TextImpl>(std::move(text));
+			return;
+		}
+		try {
+			auto formula = ParseFormula(text.substr(1));
+			impl_ = std::make_unique<FormulaImpl>(text.substr(1));
+		}
+		catch (const FormulaException&) {
+			throw;
+		}
+		return;
+	}
+
+	impl_ = std::make_unique<TextImpl>(std::move(text));
 }
 
 void Cell::Clear() {
-    impl_ = std::make_unique<EmptyImpl>();
+	impl_ = std::make_unique<EmptyImpl>();
+}
+
+bool Cell::IsReferenced() const {
+	return false;
 }
 
 Cell::Value Cell::GetValue() const {
-    return impl_->GetValue();
+	return impl_->GetValue();
 }
 
 std::string Cell::GetText() const {
-    return impl_->GetText();
+	return impl_->GetText();
+}
+
+std::vector<Position> Cell::GetReferencedCells() const {
+	return std::vector<Position>();
+}
+
+
+/*
+ * Cell::EmptyImpl
+ */
+
+Value Cell::EmptyImpl::GetValue() const {
+	return "";
+}
+
+std::string Cell::EmptyImpl::GetText() const {
+	return "";
+}
+
+
+/*
+ * Cell::TextImpl
+ */
+
+Cell::TextImpl::TextImpl(std::string text)
+	: text_(std::move(text)) {
+}
+
+Value Cell::TextImpl::GetValue() const {
+	if (text_.empty()) {
+		return "";
+	}
+
+	if (text_[0] == ESCAPE_SIGN) {
+		return text_.substr(1);
+	}
+
+	return text_;
+}
+
+std::string Cell::TextImpl::GetText() const {
+	return text_;
+}
+
+
+/*
+ * Cell::FormulaImpl
+ */
+
+Cell::FormulaImpl::FormulaImpl(std::string expression)
+	: formula_(ParseFormula(std::move(expression))) {
+}
+
+Value Cell::FormulaImpl::GetValue() const {
+	auto value = formula_->Evaluate();
+	if (std::holds_alternative<double>(value)) {
+		return std::get<double>(value);
+	}
+	else {
+		return std::get<FormulaError>(value);
+	}
+}
+
+std::string Cell::FormulaImpl::GetText() const {
+	return FORMULA_SIGN + formula_->GetExpression();
 }
--- cell.h
+++ cell.h
@@ -6,20 +6,69 @@
 
 class Cell : public CellInterface {
 public:
-    Cell();
-    ~Cell();
+	using CellDeps = std::vector<Position>;
 
-    void Set(std::string text) override;
-    void Clear();
+	Cell(Sheet& sheet);
+	~Cell() = default;
 
-    Value GetValue() const override;
-    std::string GetText() const override;
+	void Set(std::string text);
+	void Clear();
+
+	bool IsReferenced() const;
+
+	Value GetValue() const override;
+	std::string GetText() const override;
+	std::vector<Position> GetReferencedCells() const override;
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
-    
-    std::unique_ptr<Impl> impl_;
+	class Impl;
+	class EmptyImpl;
+	class TextImpl;
+	class FormulaImpl;
+
+private:
+	std::unique_ptr<Impl> impl_;
+
+	CellDeps dependencies_;
+	CellDeps dependents_;
+
+	// Êýø
+	mutable std::optional<Value> cache_;
+	mutable bool cache_valid_ = false;
+};
+
+class Cell::Impl {
+public:
+	virtual ~Impl() = default;
+
+	virtual Value GetValue() const = 0;
+	virtual std::string GetText() const = 0;
+};
+
+class Cell::EmptyImpl : public Cell::Impl {
+public:
+	Value GetValue() const override;
+	std::string GetText() const override;
+};
+
+class Cell::TextImpl : public Cell::Impl {
+public:
+	TextImpl(std::string text);
+
+	Value GetValue() const override;
+	std::string GetText() const override;
+
+private:
+	std::string text_;
+};
+
+class Cell::FormulaImpl : public Cell::Impl {
+public:
+	FormulaImpl(std::string expression);
+
+	Value GetValue() const override;
+	std::string GetText() const override;
+
+private:
+	std::unique_ptr<FormulaInterface> formula_;
 };

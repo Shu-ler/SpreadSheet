fb8f644d2c7cbec361b8d6e1a57ba679071f3002
Shu-ler <shuler1970@gmail.com>
2026-02-19 14:45:02 +0700

    FormulaAST.* из задания

--- FormulaAST.h
+++ FormulaAST.h
@@ -8,7 +8,7 @@
 #include <stdexcept>
 
 namespace ASTImpl {
-class Expr;
+    class Expr;
 }
 
 class ParsingError : public std::runtime_error {
@@ -23,16 +23,25 @@ public:
     FormulaAST& operator=(FormulaAST&&) = default;
     ~FormulaAST();
 
-    double Execute() const;
+    double Execute(/*добавьте нужные аргументы*/ args) const;
+    void PrintCells(std::ostream& out) const;
     void Print(std::ostream& out) const;
     void PrintFormula(std::ostream& out) const;
-    void PrintCells(std::ostream& out) const;
 
-    std::forward_list<Position>& GetCells();
-    const std::forward_list<Position>& GetCells() const;
+    std::forward_list<Position>& GetCells() {
+        return cells_;
+    }
+
+    const std::forward_list<Position>& GetCells() const {
+        return cells_;
+    }
 
 private:
     std::unique_ptr<ASTImpl::Expr> root_expr_;
+
+    // physically stores cells so that they can be
+    // efficiently traversed without going through
+    // the whole AST
     std::forward_list<Position> cells_;
 };
 
